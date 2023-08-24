54b2de8466b1c0bdfd03ce950e684980d6beceed
Sharon Sayaji <95227783+Sharon-03@users.noreply.github.com>
2023-08-24 11:28:42 +0530

    Create Mux.v

---
+++ Mux.v
@@ -0,0 +1,12 @@
+module mux(
+    input i0,
+    input i1,
+    input i2,
+    input i3,
+    input s0,
+    input s1,
+    output y
+    );
+	 assign y=((i0&(~s0))&(~s1))|(i1&(~s1))&s0|(i2&s1&(~s0))|(i3&s0&s1);
+
+endmodule
