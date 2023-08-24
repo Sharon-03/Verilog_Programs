92afc4d565f7cf4109d049b6a5999d19fd29dd49
Sharon Sayaji <95227783+Sharon-03@users.noreply.github.com>
2023-08-24 11:26:02 +0530

    Create JK_FlipFlop.v

---
+++ JK_FlipFlop.v
@@ -0,0 +1,20 @@
+module jkff(
+    input j, k, clk,
+    output reg q, qb
+    );
+    
+    initial 
+    q=1'b0;
+
+    always @(posedge clk)
+    begin
+        case({j,k})
+            2'b00:q=q;
+            2'b01:q=1'b0;
+            2'b10:q=1'b1;
+            2'b11:q=(~q);
+        endcase
+    qb=(~q);
+    end
+    
+endmodule
